7e08ec2fb85c9fa4c21bc16fd0cb0565089d173f
Adam Garrett-Harris <agarrharr@users.noreply.github.com>
2022-10-04 17:13:40 -0600

    Move test logic to a separate type `HTTPClientSpy`

--- EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -1,30 +1,37 @@
 import XCTest
 
 class RemoteFeedLoader {
-    let client = HTTPClient.shared
-    
     func load() {
-        client.requestedURL = URL(string: "https://a.com")
+        HTTPClient.shared.get(from: URL(string: "https://a.com")!)
     }
 }
 
 class HTTPClient {
-    static let shared = HTTPClient()
+    static var shared = HTTPClient()
+    
+    func get(from url: URL) { }
+}
+
+class HTTPClientSpy: HTTPClient {
     var requestedURL: URL?
     
-    private init() { }
+    override func get(from url: URL) {
+        requestedURL = url
+    }
 }
 
 class RemoteFeedLoaderTests: XCTestCase {
     func test_doesNotRequestDataFromURL() {
-        let client = HTTPClient.shared
-        let _ = RemoteFeedLoader()
+        let client = HTTPClientSpy()
+        HTTPClientSpy.shared = client
+        _ = RemoteFeedLoader()
         
         XCTAssertNil(client.requestedURL)
     }
     
     func test_load_requestDataFromURL() {
-        let client = HTTPClient.shared
+        let client = HTTPClientSpy()
+        HTTPClientSpy.shared = client
         let sut = RemoteFeedLoader()
         
         sut.load()
