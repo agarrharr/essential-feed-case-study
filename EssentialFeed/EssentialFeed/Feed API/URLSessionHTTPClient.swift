adebb61006b8d89340069e8e84098d683c133f0e
Adam Garrett-Harris <agarrharr@users.noreply.github.com>
2022-10-22 14:36:30 -0600

    Revert last commit because I'm concerned we might have a method conflict at some point if they ever create a method called `get(from url:)`
    My personal preference is to create my own adapter types
    
    This reverts commit 4b12e936f9464d75e2c937b6bb5e87eddba2d2b3.

--- EssentialFeed/EssentialFeed/Feed API/URLSessionHTTPClient.swift
+++ EssentialFeed/EssentialFeed/Feed API/URLSessionHTTPClient.swift
@@ -1,10 +1,16 @@
 import Foundation
 
-extension URLSession: HTTPClient {
+public class URLSessionHTTPClient: HTTPClient {
+    private let session: URLSession
+    
+    public init(session: URLSession = .shared) {
+        self.session = session
+    }
+    
     private struct UnexpectedValuesRepresentation: Error {}
     
     public func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void) {
-        dataTask(with: url) { data, response, error in
+        session.dataTask(with: url) { data, response, error in
             if let error = error {
                 completion(.failure(error))
             } else if let data = data, let response = response as? HTTPURLResponse {
--- EssentialFeed/EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
@@ -75,9 +75,9 @@ class URLSessionHTTPClientTests: XCTestCase {
     // MARK: - Helpers
     
     private func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> HTTPClient {
-        let sut = URLSession.shared
+        let sut = URLSessionHTTPClient()
         
-//        trackForMemoryLeaks(sut, file: file, line: line)
+        trackForMemoryLeaks(sut, file: file, line: line)
         
         return sut
     }
