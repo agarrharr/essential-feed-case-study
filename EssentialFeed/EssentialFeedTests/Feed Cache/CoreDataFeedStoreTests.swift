afaeae7b41782e3e794c414af9a9e7abe08270ef
Adam Garrett-Harris <agarrharr@users.noreply.github.com>
2023-05-02 12:21:03 -0600

    Whitespace change

--- EssentialFeed/EssentialFeedTests/Feed Cache/CoreDataFeedStoreTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/CoreDataFeedStoreTests.swift
@@ -2,52 +2,52 @@ import Foundation
 
 import XCTest
 
- class CoreDataFeedStoreTests: XCTestCase, FeedStoreSpecs {
-     func test_retrieve_deliversEmptyOnEmptyCache() {
-
-     }
-
-     func test_retrieve_hasNoSideEffectsOnEmptyCache() {
-
-     }
-
-     func test_retrieve_deliversFoundValuesOnNonEmptyCache() {
-
-     }
-
-     func test_retrieve_hasNoSideEffectsOnNonEmptyCache() {
-
-     }
-
-     func test_insert_deliversNoErrorOnEmptyCache() {
-
-     }
-
-     func test_insert_deliversNoErrorOnNonEmptyCache() {
-
-     }
-
-     func test_insert_overridesPreviouslyInsertedCacheValues() {
-
-     }
-
-     func test_delete_deliversNoErrorOnEmptyCache() {
-
-     }
-
-     func test_delete_hasNoSideEffectsOnEmptyCache() {
-
-     }
-
-     func test_delete_deliversNoErrorOnNonEmptyCache() {
-
-     }
-
-     func test_delete_emptiesPreviouslyInsertedCache() {
-
-     }
-
-     func test_storeSideEffects_runSerially() {
-
-     }
- }
+class CoreDataFeedStoreTests: XCTestCase, FeedStoreSpecs {
+    func test_retrieve_deliversEmptyOnEmptyCache() {
+        
+    }
+    
+    func test_retrieve_hasNoSideEffectsOnEmptyCache() {
+        
+    }
+    
+    func test_retrieve_deliversFoundValuesOnNonEmptyCache() {
+        
+    }
+    
+    func test_retrieve_hasNoSideEffectsOnNonEmptyCache() {
+        
+    }
+    
+    func test_insert_deliversNoErrorOnEmptyCache() {
+        
+    }
+    
+    func test_insert_deliversNoErrorOnNonEmptyCache() {
+        
+    }
+    
+    func test_insert_overridesPreviouslyInsertedCacheValues() {
+        
+    }
+    
+    func test_delete_deliversNoErrorOnEmptyCache() {
+        
+    }
+    
+    func test_delete_hasNoSideEffectsOnEmptyCache() {
+        
+    }
+    
+    func test_delete_deliversNoErrorOnNonEmptyCache() {
+        
+    }
+    
+    func test_delete_emptiesPreviouslyInsertedCache() {
+        
+    }
+    
+    func test_storeSideEffects_runSerially() {
+        
+    }
+}
