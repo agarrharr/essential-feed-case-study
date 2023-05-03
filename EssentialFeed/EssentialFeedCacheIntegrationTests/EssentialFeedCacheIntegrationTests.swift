abc6c7024b35a58aa4dcdc88fd7e9309bca58d5f
Adam Garrett-Harris <agarrharr@users.noreply.github.com>
2023-05-03 12:03:58 -0600

    Test the `CoreDataFeedStore` in the `EssentialCacheIntegrationTests` instead of the `CodableFeedStore`

--- EssentialFeed/EssentialFeedCacheIntegrationTests/EssentialFeedCacheIntegrationTests.swift
+++ EssentialFeed/EssentialFeedCacheIntegrationTests/EssentialFeedCacheIntegrationTests.swift
@@ -47,7 +47,8 @@ class EssentialFeedCacheIntegrationTests: XCTestCase {
     
     private func makeSUT(file: StaticString = #file, line: UInt = #line) -> LocalFeedLoader {
         let storeURL = testSpecificStoreURL()
-        let store = CodableFeedStore(storeURL: storeURL)
+        let storeBundle = Bundle(for: CoreDataFeedStore.self)
+        let store = try! CoreDataFeedStore(storeURL: storeURL, bundle: storeBundle)
         let sut = LocalFeedLoader(store: store, currentDate: Date.init)
         trackForMemoryLeaks(store, file: file, line: line)
         trackForMemoryLeaks(sut, file: file, line: line)
