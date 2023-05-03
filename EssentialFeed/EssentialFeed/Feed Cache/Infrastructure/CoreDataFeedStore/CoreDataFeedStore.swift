f9538553587438668ef72f31134a56df3a44040e
Adam Garrett-Harris <agarrharr@users.noreply.github.com>
2023-05-03 11:17:31 -0600

    Retrieve method delivers found values on non-empty cache

--- EssentialFeed/EssentialFeed/Feed Cache/Infrastructure/CoreDataFeedStore/CoreDataFeedStore.swift
+++ EssentialFeed/EssentialFeed/Feed Cache/Infrastructure/CoreDataFeedStore/CoreDataFeedStore.swift
@@ -1,4 +1,3 @@
-import Foundation
 import CoreData
 
 public final class CoreDataFeedStore: FeedStore {
@@ -11,10 +10,49 @@ public final class CoreDataFeedStore: FeedStore {
     }
     
     public func retrieve(completion: @escaping RetrievalCompletion) {
-        completion(.empty)
+        let context = self.context
+        context.perform {
+            do {
+                let request = NSFetchRequest<ManagedCache>(entityName: ManagedCache.entity().name!)
+                request.returnsObjectsAsFaults = false
+                if let cache = try context.fetch(request).first {
+                    completion(.found(
+                        feed: cache.feed
+                            .compactMap { ($0 as? ManagedFeedImage) }
+                            .map {
+                                LocalFeedImage(id: $0.id, description: $0.imageDescription, location: $0.location, url: $0.url)
+                            },
+                        timestamp: cache.timestamp))
+                } else {
+                    completion(.empty)
+                }
+            } catch {
+                completion(.failure(error))
+            }
+        }
     }
     
     public func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
+        let context = self.context
+        context.perform {
+            do {
+                let managedCache = ManagedCache(context: context)
+                managedCache.timestamp = timestamp
+                managedCache.feed = NSOrderedSet(array: feed.map { local in
+                    let managed = ManagedFeedImage(context: context)
+                    managed.id = local.id
+                    managed.imageDescription = local.description
+                    managed.location = local.location
+                    managed.url = local.url
+                    return managed
+                })
+                
+                try context.save()
+                completion(nil)
+            } catch {
+                completion(error)
+            }
+        }
     }
     
     public func deleteCachedFeed(completion: @escaping DeletionCompletion) {
@@ -22,15 +60,16 @@ public final class CoreDataFeedStore: FeedStore {
 }
 
 private extension NSPersistentContainer {
-    enum LoadingError: Error {
+    enum LoadingError: Swift.Error {
         case modelNotFound
-        case failedToLoadPersistentStores(Error)
+        case failedToLoadPersistentStores(Swift.Error)
     }
     
     static func load(modelName name: String, url: URL, in bundle: Bundle) throws -> NSPersistentContainer {
         guard let model = NSManagedObjectModel.with(name: name, in: bundle) else {
             throw LoadingError.modelNotFound
         }
+        
         let description = NSPersistentStoreDescription(url: url)
         let container = NSPersistentContainer(name: name, managedObjectModel: model)
         container.persistentStoreDescriptions = [description]
@@ -38,6 +77,7 @@ private extension NSPersistentContainer {
         var loadError: Swift.Error?
         container.loadPersistentStores { loadError = $1 }
         try loadError.map { throw LoadingError.failedToLoadPersistentStores($0) }
+        
         return container
     }
 }
@@ -50,15 +90,17 @@ private extension NSManagedObjectModel {
     }
 }
 
+@objc(ManagedCache)
 private class ManagedCache: NSManagedObject {
     @NSManaged var timestamp: Date
     @NSManaged var feed: NSOrderedSet
 }
 
-private class ManagedImageFeed: NSManagedObject {
+@objc(ManagedFeedImage)
+private class ManagedFeedImage: NSManagedObject {
     @NSManaged var id: UUID
-    @NSManaged var imageDescription: String
-    @NSManaged var location: String
+    @NSManaged var imageDescription: String?
+    @NSManaged var location: String?
     @NSManaged var url: URL
-    @NSManaged var cache: NSOrderedSet
+    @NSManaged var cache: ManagedCache
 }
