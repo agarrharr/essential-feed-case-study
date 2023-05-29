ae7f2f130731b41cd3e84e58d78bda59992346e1
Adam Garrett-Harris <agarrharr@users.noreply.github.com>
2023-05-29 11:15:06 -0600

    Add prototype feed data for demoing purposes

--- Prototype/Prototype/FeedViewController.swift
+++ Prototype/Prototype/FeedViewController.swift
@@ -1,5 +1,11 @@
 import UIKit
 
+struct FeedImageViewModel {
+    let description: String?
+    let location: String?
+    let imageName: String
+}
+
 final class FeedViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 10
